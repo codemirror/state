3db8770b550e619e5ec26d591a0d51e19a0301b6
Marijn Haverbeke <marijn@haverbeke.nl>
2021-10-01 10:33:32 +0200

    Add a strict argument to findColumn
    To be able to distinguish between the end of the string and columns beyond it.

--- src/column.ts
+++ src/column.ts
@@ -17,12 +17,16 @@ export function countColumn(string: string, tabSize: number, to = string.length)
 }
 
 /// Find the offset that corresponds to the given column position in a
-/// string, taking extending characters and tab size into account.
-export function findColumn(string: string, col: number, tabSize: number): number {
-  for (let i = 0, n = 0; i < string.length;) {
+/// string, taking extending characters and tab size into account. By
+/// default, the string length is returned when it is too short to
+/// reach the column. Pass `strict` true to make it return -1 in that
+/// situation.
+export function findColumn(string: string, col: number, tabSize: number, strict?: boolean): number {
+  for (let i = 0, n = 0;;) {
     if (n >= col) return i
+    if (i == string.length) break
     n += string.charCodeAt(i) == 9 ? tabSize - (n % tabSize) : 1
     i = findClusterBreak(string, i)
   }
-  return string.length
+  return strict === true ? -1 : string.length
 }
